c8c32fcc10ae752ef837a1fa4d312c9bdc69d79f
Ali <aligoni123@hotmail.com>
2023-02-17 16:50:25 +0000

    Changed to emp.tostring method instead of getter methods

--- src/main/java/com/sparta/view/Output.java
+++ src/main/java/com/sparta/view/Output.java
@@ -4,29 +4,21 @@ import com.sparta.model.Employee;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.logging.Level;
 
 public class Output {
-    public static void displayEmployeeInfo(Employee[] employees){
+    public static void displayEmployeeInfo(ArrayList<Employee> employees){
         String dateTime = LocalDateTime.now().format(DateTimeFormatter.ISO_LOCAL_DATE_TIME);
         logger.log(Level.ALL,dateTime);
         logger.log(Level.ALL, UserInput.getInput());
         logger.log(Level.ALL, "Results found: " + employees.length);
         logger.log(Level.ALL, " ");
         for (Employee emp: employees){
-            logger.log(Level.ALL,emp.getID());
-            logger.log(Level.ALL,emp.getfirstName());
-            logger.log(Level.ALL,emp.getlastName());
-            logger.log(Level.ALL,emp.getHireDate());
-            logger.log(Level.ALL,emp.getBirthdate());
-            logger.log(Level.ALL,emp.getGender());
-            logger.log(Level.ALL, "");
+            logger.log(Level.ALL,emp.toString());;
         }
 
     }
 
-    public static void main(String[] args) {
-        displayEmployeeInfo();
-    }
 }
