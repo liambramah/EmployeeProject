a7336fc3607af62b82753e5509142ef043e04c7c
Honghao Zheng <hxz157@cs.bham.ac.uk>
2023-02-17 15:24:56 +0000

    model init

--- src/main/java/com/sparta/model/Model.java
+++ src/main/java/com/sparta/model/Model.java
@@ -1,4 +1,24 @@
 package com.sparta.model;
 
+import com.sparta.employee.Employee;
+import com.sun.source.tree.BinaryTree;
+
+import java.util.ArrayList;
+
 public class Model {
+    BinaryTree collectionOfEmployees;
+    public ArrayList<Employee> addEmployees(){
+
+    }
+    public ArrayList<Employee> retriveEmployees(String lastName){
+        ArrayList<Employee> listOfEmployees= addEmployees();
+        ArrayList<Employee> selectedEmploees=ArrayList<Employee>();
+        for(Employee e:listOfEmployees){
+            if(e.getLastName.equals(lastName)){
+                selectedEmploees.add(e)
+            }
+        }
+        return selectedEmploees;
+
+    }
 }
