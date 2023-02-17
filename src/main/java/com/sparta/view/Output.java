5f3ce14535101ff25b2d8aca9eb10175f82235f5
Ali <aligoni123@hotmail.com>
2023-02-17 14:52:59 +0000

    Userinput method and displayEmployeeInfo methods created

--- src/main/java/com/sparta/view/Output.java
+++ src/main/java/com/sparta/view/Output.java
@@ -1,4 +1,32 @@
 package com.sparta.view;
 
+import com.sparta.model.Employee;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+import java.util.logging.Level;
+
 public class Output {
+    public static void displayEmployeeInfo(Employee[] employees){
+        String dateTime = LocalDateTime.now().format(DateTimeFormatter.ISO_LOCAL_DATE_TIME);
+        logger.log(Level.ALL,dateTime);
+        logger.log(Level.ALL, UserInput.getInput());
+        logger.log(Level.ALL, "Results found: " + employees.length);
+        logger.log(Level.ALL, " ");
+        for (Employee emp: employees){
+            logger.log(Level.ALL,emp.getID());
+            logger.log(Level.ALL,emp.getfirstName());
+            logger.log(Level.ALL,emp.getlastName());
+            logger.log(Level.ALL,emp.getHireDate());
+            logger.log(Level.ALL,emp.getBirthdate());
+            logger.log(Level.ALL,emp.getGender());
+            logger.log(Level.ALL, "");
+        }
+
+    }
+
+    public static void main(String[] args) {
+        displayEmployeeInfo();
+    }
 }
--- src/main/java/com/sparta/view/UserInput.java
+++ src/main/java/com/sparta/view/UserInput.java
@@ -1,4 +1,12 @@
 package com.sparta.view;
 
+import java.util.Scanner;
+
 public class UserInput {
+    public static String getInput(){
+        Scanner scanner = new Scanner(System.in);
+        String searchedName = scanner.nextLine();
+        return searchedName;
+    }
+
 }
