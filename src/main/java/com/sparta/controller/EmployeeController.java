e8f1e3703f86f1b8d3a3398c52991dc01a987497
Oliver Norgate <oliverpnorgate@gmail.com>
2023-02-17 16:29:53 +0000

    I have created the controller class which talks to the model and view, additionally, I have changed my pom file as it seemed to have an error in it

--- src/main/java/com/sparta/controller/EmployeeController.java
+++ src/main/java/com/sparta/controller/EmployeeController.java
@@ -1,4 +1,43 @@
 package com.sparta.controller;
 
-public class EmployeeController {
+import com.sparta.employee.Employee;
+import com.sparta.employeefactory.ConvertedEmployeeFactory;
+import com.sparta.model.Model;
+import com.sparta.view.EmployeeView;
+
+import java.util.ArrayList;
+
+public class EmployeeController{
+
+//    RetrieveEmployeesFromLastName retrieveLastName = new RetrieveEmployeesFromLastName();
+
+//    RetrieveEmployeesFromLastName retrieveLastName = new RetrieveEmployeesFromLastName();;
+
+    private Employee model;
+    private EmployeeView view;
+
+    public EmployeeController(Employee model, EmployeeView view) {
+        this.model = model;
+        this.view = view;
+    }
+
+    public void setUpEmployees(){
+
+        ArrayList<Employee> listOfEmployees = ConvertedEmployeeFactory.getEmployees(2);
+        model.addEmployees(listOfEmployees);
+    }
+
+    public ArrayList<Employee> searchLastName(String retrieveEmployee){
+        model.retrieveEmployeesFromLastName(retrieveEmployee);
+    }
+
+    public void run(){
+        this.setUpEmployees(); // We want to set this up before the user can do anything
+        view.displayPrompt();
+        String inputLastName = view.getInput();
+        ArrayList<Employee> chosenName = searchLastName(inputLastName);
+        view.displayOutput(chosenName);
+    }
+
+
 }
