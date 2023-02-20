a9800d1971d6f263001c61e91e438f04d81c1fe9
sp01186 <58943663+sp01186@users.noreply.github.com>
2023-02-20 17:32:34 +0000

    Proving it works with duplicates

--- src/main/java/com/sparta/model/Model.java
+++ src/main/java/com/sparta/model/Model.java
@@ -4,11 +4,20 @@ import com.sparta.employee.Employee;
 import com.sparta.binarytree.BinaryTree;
 
 import java.util.ArrayList;
+import java.util.Set;
+import java.util.HashSet;
 
 public class Model {
     BinaryTree collectionOfEmployees = new BinaryTree();
     public  void addEmployees(ArrayList<Employee> employees){
-        System.out.println("last name that exists is " + employees.get(0).getLastName());
+        final Set<String> set1 = new HashSet<>();
+
+        for (int i = 0; i < employees.size(); i++) {
+            if (!set1.add(employees.get(i).getLastName())) {
+                System.out.println("the duplicates are: " + employees.get(i).getLastName());
+            }
+        }
+        //System.out.println("last name that exists is " + employees.get(0).getLastName());
         this.collectionOfEmployees.addElements(employees);
     }
     public ArrayList<Employee> retrieveEmployeesFromLastName(String lastName){
