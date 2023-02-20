1d1df96fd208acb465433878fecec16d96df1179
sp01186 <58943663+sp01186@users.noreply.github.com>
2023-02-20 16:48:59 +0000

    fixed the model

--- src/main/java/com/sparta/model/Model.java
+++ src/main/java/com/sparta/model/Model.java
@@ -1,26 +1,18 @@
 package com.sparta.model;
 
 import com.sparta.employee.Employee;
-import com.sun.source.tree.BinaryTree;
+import com.sparta.binarytree.BinaryTree;
 
 import java.util.ArrayList;
 
 public class Model {
-    BinaryTree collectionOfEmployees=new BinaryTree();
+    BinaryTree collectionOfEmployees = new BinaryTree();
     public  void addEmployees(ArrayList<Employee> employees){
-            this.collectionOfEmployees.addElements(e);
+            this.collectionOfEmployees.addElements(employees);
     }
-    public ArrayList<Employee> retriveEmployees(String lastName){
-//        ArrayList<Employee> listOfEmployees= addEmployees();
-//        ArrayList<Employee> selectedEmploees=ArrayList<Employee>();
-//        selectedEmploees=collectionOfEmployees.findEmployee(lastName);
-//        for(Employee e:listOfEmployees){
-//            if(e.getLastName.equals(lastName)){
-//                selectedEmploees.add(e)
-//            }
-//        }
+    public ArrayList<Employee> retrieveEmployeesFromLastName(String lastName){
         
-        ArrayList<Employee> arrayOfEmployees = this.collectionOfEmployees.getAllElements(lastName);
+        ArrayList<Employee> arrayOfEmployees = this.collectionOfEmployees.getElement(lastName);
         return arrayOfEmployees;
 
     }
